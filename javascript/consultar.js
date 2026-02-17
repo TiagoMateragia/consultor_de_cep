065e493eb710c42804a3da6f248f7da079ab4264
TiagoMateragia <tiagomateragia27@gmail.com>
2026-02-16 22:48:51 -0300

    16/02 Integração com a API
    Integração com a API feita, faltando somente ajustar o posicionamento do resultado para o usuário.

--- javascript/consultar.js
+++ javascript/consultar.js
@@ -38,3 +38,48 @@ botao_tema.addEventListener('click', ()=> {
     }
 })
 
+//Variaveis para utilizar na função.
+let msg_erro = document.querySelector('.msg_erro');
+let logradouro = document.querySelector('.logradouro');
+let complemento = document.querySelector('.complemento');
+let unidade = document.querySelector('.unidade');
+let bairro = document.querySelector('.bairro');
+let localidade = document.querySelector('.localidade');
+let uf = document.querySelector('.uf');
+let estado = document.querySelector('.estado');
+let regiao = document.querySelector('.regiao');
+let ibge = document.querySelector('.ibge');
+let gia = document.querySelector('.gia');
+let ddd = document.querySelector('.ddd');
+let siafi = document.querySelector('.siafi');
+
+//Função para chamada da API e consultar o CEP digitado no input.
+function consultar_cep(){
+    let cep_val = cep.value;
+
+    fetch(`https://viacep.com.br/ws/${cep_val}/json/`)
+        .then((call)=> {
+            return call.json();
+        })
+        .then((call_json)=> {
+            let logradouro_val = call_json.logradouro;
+            let complemento_val = call_json.complemento;
+            let unidade_val = call_json.unidade;
+            let bairro_val = call_json.bairro;
+            let localidade_val = call_json.bairro;
+            let uf_val = call_json.uf;
+            let estado_val = call_json.estado;
+            let regiao_val = call_json.regiao;
+            let ibge_val = call_json.ibge;
+            let gia_val = call_json.gia;
+            let ddd_val = call_json.ddd;
+            let siafi_val = call_json.siafi;
+            console.log(call_json);
+        })
+        .catch((erro)=> {
+            msg_erro.innerHTML = "Não encontrado.";
+            setTimeout(()=> {
+                msg_erro.innerHTML = '';
+            }, 3000);
+        })
+}
